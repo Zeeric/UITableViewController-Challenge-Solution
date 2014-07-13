b0675607f57c86e93db82d2b2ce80928955ae053
Eric Zeinstra <mmzeinstra@Erics-MacBook-Pro.local>
2014-07-13 13:48:26 -0400

    solved the challenge

---
+++ UITableViewController Challenge Solution/ZEETableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ZEETableViewController.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Eric Zeinstra on 7/13/14.
+//  Copyright (c) 2014 Eric Zeinstra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZEETableViewController : UITableViewController
+
+@end
